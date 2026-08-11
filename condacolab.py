0d5d4a8a7675e013d0c0c444c427814297471536
jaimergp <jaimergp@users.noreply.github.com>
2026-08-11 20:43:50 +0200

    Format

--- condacolab.py
+++ condacolab.py
@@ -44,7 +44,7 @@ PREFIX = "/opt/conda"
 
 
 restart_kernel_button = widgets.Button(description="Restart kernel now...")
-restart_button_output = widgets.Output(layout={'border': '1px solid black'})
+restart_button_output = widgets.Output(layout={"border": "1px solid black"})
 
 
 def _on_button_clicked(b):
@@ -52,6 +52,7 @@ def _on_button_clicked(b):
         get_ipython().kernel.do_shutdown(True)
         print("Kernel restarted!")
         restart_kernel_button.close()
+
 
 def _run_subprocess(command, logs_filename):
     """
@@ -67,19 +68,21 @@ def _run_subprocess(command, logs_filename):
     """
 
     task = run(
-            command,
-            check=False,
-            stdout=PIPE,
-            stderr=STDOUT,
-            text=True,
-        )
+        command,
+        check=False,
+        stdout=PIPE,
+        stderr=STDOUT,
+        text=True,
+    )
 
     logs_file_path = "/var/condacolab"
     os.makedirs(logs_file_path, exist_ok=True)
 
     with open(f"{logs_file_path}/{logs_filename}", "w") as f:
         f.write(task.stdout)
-    assert (task.returncode == 0), f"💥💔💥 The installation failed! Logs are available at `{logs_file_path}/{logs_filename}`."
+    assert task.returncode == 0, (
+        f"💥💔💥 The installation failed! Logs are available at `{logs_file_path}/{logs_filename}`."
+    )
 
 
 def install_from_url(
@@ -117,8 +120,8 @@ def install_from_url(
         Change to False to ignore checks and always attempt
         to run the installation.
     restart_kernel
-        Variable to manage the kernel restart during the installation 
-        of condacolab. Set it `False` to stop the kernel from restarting 
+        Variable to manage the kernel restart during the installation
+        of condacolab. Set it `False` to stop the kernel from restarting
         automatically and get a button instead to do it.
     """
     if run_checks:
@@ -136,7 +139,7 @@ def install_from_url(
     condacolab_task = _run_subprocess(
         ["bash", installer_fn, "-bfp", str(prefix)],
         "condacolab_install.log",
-        )
+    )
 
     print("📌 Adjusting configuration...")
     cuda_version = ".".join(os.environ.get("CUDA_VERSION", "*.*.*").split(".")[:2])
@@ -152,11 +155,11 @@ def install_from_url(
 
     print("📦 Installing...")
 
-# Installing the following packages because Colab server expects these packages to be installed in order to launch a Python kernel:
-#     - matplotlib-base
-#     - psutil
-#     - google-colab
-#     - colabtools
+    # Installing the following packages because Colab server expects these packages to be installed in order to launch a Python kernel:
+    #     - matplotlib-base
+    #     - psutil
+    #     - google-colab
+    #     - colabtools
 
     conda_exe = "mamba" if os.path.isfile(f"{prefix}/bin/mamba") else "conda"
 
@@ -164,7 +167,7 @@ def install_from_url(
 
     output = check_output([f"{prefix}/bin/conda", "list", "--json"])
     payload = json.loads(output)
-    installed_names = [pkg["name"] for pkg in payload] 
+    installed_names = [pkg["name"] for pkg in payload]
     required_packages = ["matplotlib-base", "psutil", "google-colab"]
     for pkg in required_packages.copy():
         if pkg in installed_names:
@@ -177,9 +180,18 @@ def install_from_url(
         )
 
     pip_task = _run_subprocess(
-        [f"{prefix}/bin/python", "-m", "pip", "-q", "install", "-U", "https://github.com/googlecolab/colabtools/archive/refs/heads/main.zip", "condacolab"],
-        "pip_task.log"
-        )
+        [
+            f"{prefix}/bin/python",
+            "-m",
+            "pip",
+            "-q",
+            "install",
+            "-U",
+            "https://github.com/googlecolab/colabtools/archive/refs/heads/main.zip",
+            "condacolab",
+        ],
+        "pip_task.log",
+    )
 
     env = env or {}
     bin_path = f"{prefix}/bin"
@@ -223,7 +235,10 @@ def install_mambaforge(*args, **kwargs):
 
 
 def install_miniforge(
-    prefix: os.PathLike = PREFIX, env: Dict[AnyStr, AnyStr] = None, run_checks: bool = True, restart_kernel: bool = True,
+    prefix: os.PathLike = PREFIX,
+    env: Dict[AnyStr, AnyStr] = None,
+    run_checks: bool = True,
+    restart_kernel: bool = True,
 ):
     """
     Install Mambaforge, built for Python 3.7.
@@ -252,12 +267,18 @@ def install_miniforge(
         Change to False to ignore checks and always attempt
         to run the installation.
     restart_kernel
-        Variable to manage the kernel restart during the installation 
-        of condacolab. Set it `False` to stop the kernel from restarting 
+        Variable to manage the kernel restart during the installation
+        of condacolab. Set it `False` to stop the kernel from restarting
         automatically and get a button instead to do it.
     """
     installer_url = r"https://github.com/jaimergp/miniforge/releases/latest/download/Miniforge-colab-Linux-x86_64.sh"
-    install_from_url(installer_url, prefix=prefix, env=env, run_checks=run_checks, restart_kernel=restart_kernel)
+    install_from_url(
+        installer_url,
+        prefix=prefix,
+        env=env,
+        run_checks=run_checks,
+        restart_kernel=restart_kernel,
+    )
 
 
 # Make miniforge the default
@@ -265,7 +286,10 @@ install = install_miniforge
 
 
 def install_miniconda(
-    prefix: os.PathLike = PREFIX, env: Dict[AnyStr, AnyStr] = None, run_checks: bool = True, restart_kernel: bool = True,
+    prefix: os.PathLike = PREFIX,
+    env: Dict[AnyStr, AnyStr] = None,
+    run_checks: bool = True,
+    restart_kernel: bool = True,
 ):
     """
     Install Miniconda 4.12.0 for Python 3.7.
@@ -289,16 +313,27 @@ def install_miniconda(
         Change to False to ignore checks and always attempt
         to run the installation.
     restart_kernel
-        Variable to manage the kernel restart during the installation 
-        of condacolab. Set it `False` to stop the kernel from restarting 
+        Variable to manage the kernel restart during the installation
+        of condacolab. Set it `False` to stop the kernel from restarting
         automatically and get a button instead to do it.
     """
-    installer_url = r"https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh"
-    install_from_url(installer_url, prefix=prefix, env=env, run_checks=run_checks, restart_kernel=restart_kernel)
+    installer_url = (
+        r"https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh"
+    )
+    install_from_url(
+        installer_url,
+        prefix=prefix,
+        env=env,
+        run_checks=run_checks,
+        restart_kernel=restart_kernel,
+    )
 
 
 def install_anaconda(
-    prefix: os.PathLike = PREFIX, env: Dict[AnyStr, AnyStr] = None, run_checks: bool = True, restart_kernel: bool = True,
+    prefix: os.PathLike = PREFIX,
+    env: Dict[AnyStr, AnyStr] = None,
+    run_checks: bool = True,
+    restart_kernel: bool = True,
 ):
     """
     Install Anaconda 2022.05, the latest version built
@@ -323,12 +358,20 @@ def install_anaconda(
         Change to False to ignore checks and always attempt
         to run the installation.
     restart_kernel
-        Variable to manage the kernel restart during the installation 
-        of condacolab. Set it `False` to stop the kernel from restarting 
+        Variable to manage the kernel restart during the installation
+        of condacolab. Set it `False` to stop the kernel from restarting
         automatically and get a button instead to do it.
     """
-    installer_url = r"https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh"
-    install_from_url(installer_url, prefix=prefix, env=env, run_checks=run_checks, restart_kernel=restart_kernel)
+    installer_url = (
+        r"https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh"
+    )
+    install_from_url(
+        installer_url,
+        prefix=prefix,
+        env=env,
+        run_checks=run_checks,
+        restart_kernel=restart_kernel,
+    )
 
 
 def check(prefix: os.PathLike = PREFIX, verbose: bool = True):
@@ -348,16 +391,18 @@ def check(prefix: os.PathLike = PREFIX, verbose: bool = True):
 
     pymaj, pymin = sys.version_info[:2]
     sitepackages = f"{prefix}/lib/python{pymaj}.{pymin}/site-packages"
-    assert sitepackages in sys.path, f"💥💔💥 PYTHONPATH was not patched! Value: {sys.path}"
-    assert all(
-        not path.startswith("/usr/local/") for path in sys.path
-    ), f"💥💔💥 PYTHONPATH include system locations: {[path for path in sys.path if path.startswith('/usr/local')]}!"
-    assert (
-        f"{prefix}/bin" in os.environ["PATH"]
-    ), f"💥💔💥 PATH was not patched! Value: {os.environ['PATH']}"
-    assert (
-        prefix == os.environ.get("CONDA_PREFIX")
-    ), f"💥💔💥 CONDA_PREFIX value: {os.environ.get('CONDA_PREFIX', '<not set>')} does not match conda installation location {prefix}!"
+    assert sitepackages in sys.path, (
+        f"💥💔💥 PYTHONPATH was not patched! Value: {sys.path}"
+    )
+    assert all(not path.startswith("/usr/local/") for path in sys.path), (
+        f"💥💔💥 PYTHONPATH include system locations: {[path for path in sys.path if path.startswith('/usr/local')]}!"
+    )
+    assert f"{prefix}/bin" in os.environ["PATH"], (
+        f"💥💔💥 PATH was not patched! Value: {os.environ['PATH']}"
+    )
+    assert prefix == os.environ.get("CONDA_PREFIX"), (
+        f"💥💔💥 CONDA_PREFIX value: {os.environ.get('CONDA_PREFIX', '<not set>')} does not match conda installation location {prefix}!"
+    )
 
     if verbose:
         print("✨🍰✨ Everything looks OK!")
